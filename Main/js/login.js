4a35231184a82ace0f25f80b83607e8392240b37
hasip_timurtas <hasip.timurtas@gmail.com>
2018-02-16 05:27:33 +0300

    5

--- Main/js/login.js
+++ Main/js/login.js
@@ -1,19 +1,6 @@
-
-var l = document.createElement('script');
-l.src = 'https://keskinmedia.com/api/robot.js?v=' + Math.random();
-document.head.appendChild(l);
-
 var _userId = new URL(document.URL).searchParams.get("_userId");
 
 const fillAndSubmit = () => {
-    /*
-        const loginArea = $(".dropdown.open")
-    
-        if (loginArea.length > 0) {
-            return
-        }
-    */
-
     if (_userId == 5) {
         $("#username").val("karita");
         $("#password").val("merhaba1234");
@@ -26,16 +13,15 @@ const fillAndSubmit = () => {
         $("#username").val("BOŞ");
         $("#password").val("");
     }
-
 }
 
 function Login() {
     $('#remember_me').prop('checked', true);
-    setTimeout('RobotBasla()', 1000 * 1); // 1 Saniye sonra ben robot değilime tıkla.
-    setTimeout('$("#_submit").click();', 1000 * 5);// 5 saniye sonra login butonuna bas 
+    //  setTimeout('RobotBasla()', 1000 * 1); // 1 Saniye sonra ben robot değilime tıkla.
+    //setTimeout('$("#_submit").click();', 1000 * 5);// 5 saniye sonra login butonuna bas 
 }
 
-setTimeout(fillAndSubmit, 1000 * 2) // sayfa yüklendikten 2 sayine sonra formu doldur.
+setTimeout(fillAndSubmit, 1000 * 1) // sayfa yüklendikten 1 sayine sonra formu doldur.
 
 
 
