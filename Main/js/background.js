759e766d2da7bc8b098bbc8decc0fbfdc3ee495a
hasip_timurtas <hasip.timurtas@gmail.com>
2018-03-24 14:08:03 +0300

    backgrounda firebase js fonksiyonları eklendi

--- Main/js/background.js
+++ Main/js/background.js
@@ -797,10 +797,28 @@ function wsMarketData(market_id) {
     };
 }
 
-
 async function getYobitHistory(){
     var search = `draw=1&columns%5B0%5D%5Bdata%5D=0&columns%5B0%5D%5Bname%5D=&columns%5B0%5D%5Bsearchable%5D=true&columns%5B0%5D%5Borderable%5D=false&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=1&columns%5B1%5D%5Bname%5D=&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=false&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=2&columns%5B2%5D%5Bname%5D=&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=false&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=3&columns%5B3%5D%5Bname%5D=&columns%5B3%5D%5Bsearchable%5D=true&columns%5B3%5D%5Borderable%5D=false&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B4%5D%5Bdata%5D=4&columns%5B4%5D%5Bname%5D=&columns%5B4%5D%5Bsearchable%5D=true&columns%5B4%5D%5Borderable%5D=false&columns%5B4%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B4%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B5%5D%5Bdata%5D=5&columns%5B5%5D%5Bname%5D=&columns%5B5%5D%5Bsearchable%5D=true&columns%5B5%5D%5Borderable%5D=false&columns%5B5%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B5%5D%5Bsearch%5D%5Bregex%5D=false&start=0&length=105&search%5Bvalue%5D=&search%5Bregex%5D=false&action=bids&pair_id=0&currency_id=0&csrf_token=` + $('#csrf_token').val();
     var params = JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}');
     var result = await $.post('https://yobit.net/ajax/system_history.php', params).then()
     return result
+}
+
+async function LoadFireBase(){
+    await $.getScript('https://www.gstatic.com/firebasejs/4.12.0/firebase.js')
+    LoadFireBaseConfig()
+    return "Yüklendi";
+}
+
+function LoadFireBaseConfig() {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDxDY2_n2XA4mF3RWTFXRuu0XrLCkYYG4s",
+        authDomain: "firem-b3432.firebaseapp.com",
+        databaseURL: "https://firem-b3432.firebaseio.com",
+        projectId: "firem-b3432",
+        storageBucket: "firem-b3432.appspot.com",
+        messagingSenderId: "866789153670"
+    };
+    firebase.initializeApp(config);
 }
